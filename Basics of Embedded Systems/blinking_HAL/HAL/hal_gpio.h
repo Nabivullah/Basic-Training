b66ac76e85fc7c90776e94970486ca0bf8640727
Nabivullah <Syed.Nabivullah@esyasoft.com>
2025-09-14 17:52:47 +0530

    first commit

---
+++ Basics of Embedded Systems/blinking_HAL/HAL/hal_gpio.h
@@ -0,0 +1,14 @@
+#ifndef HAL_GPIO
+#include HAL_GPIO
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+#define input 0
+#define output 1
+
+void hal_gpio_init(){}
+void hal_gpio_write(){}
+void hal_gpio_read(){}
+
+#endif HAL_GPIO
---
+++ Basics of Embedded Systems/blinking_HAL/config/pin_config.h
@@ -0,0 +1,2 @@
+#define BUTTON_PIN PD2
+#define LED_PIN PB5
