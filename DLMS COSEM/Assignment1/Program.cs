2691e6fc3486974ef9e5eae8b381d0124f9fd965
Nabivullah <Syed.Nabivullah@esyasoft.com>
2025-09-17 17:53:57 +0530

    DLMS Assignment 1

---
+++ DLMS COSEM/Assignment1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;   
+
+namespace DLMS_PROJ1
+{
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                // OBIS code for Active Energy Import (1-0:1.8.0.255)
+                var obis_code = new ObisCode(1, 0, 1, 8, 0, 255);
+
+                // Class ID for Register object is 3
+                ushort class_Id = 3;
+                byte Attribute_Id = 2; // e.g., value attribute
+
+                byte[] apdu = GetRequestApdu.CreateGetRequest(obis_code, class_Id, Attribute_Id);
+
+                Console.WriteLine("OBIS Code: " + obis_code);
+                Console.WriteLine("GET Request APDU: " + BitConverter.ToString(apdu));
+            }
+        }
+}
