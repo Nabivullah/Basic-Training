70af18153c6bf440943c2c8250817fab194e365d
Nabivullah <Syed.Nabivullah@esyasoft.com>
2025-09-17 16:51:02 +0530

    first commit

---
+++ .NET/BANK_APP_USING_CLASS_FOR_SINGLE_ACCOUNT/BANK_APP_WITH_CLASS/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.Security.Principal;
+
+namespace BANK_APP_USING_CLASS_FOR_SINGLE_ACCOUNT
+{
+    internal class Program
+    {
+        static public int option;
+            
+            int acc_no = 0;
+            string acc_name = string.Empty;
+            int balance = 0;
+            int deposit;
+
+            static void display_menu()
+            {
+                Console.WriteLine("1. Create a New Bank Account");
+                Console.WriteLine("2. Deposit Money");
+                Console.WriteLine("3. Withdraw Money");
+                Console.WriteLine("4. Display account details");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Please enter your option");
+                option = Convert.ToInt32(Console.ReadLine());
+            }
+            static void Main(string[] args)
+            {
+                //create account based bank management system for one account using classes
+
+
+                display_menu();
+                account new_account = new account();
+                int acc_no = 0;
+                string acc_name = string.Empty;
+                int balance = 0;
+                int deposit = 0;
+                while (option > 5)
+                {
+                    Console.WriteLine("Invalid Option");
+                    Console.WriteLine("Please enter valid option");
+                    option = Convert.ToInt32(Console.ReadLine());
+
+                }
+                while (option != 5)
+                {
+                    if (option == 1)
+                    {
+                        Console.WriteLine("Enter Account Number ");
+                        acc_no = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Account Holder Name ");
+                        acc_name = Console.ReadLine();
+                        new_account = new account(acc_no, acc_name, balance);
+
+
+                        display_menu();
+                    }
+                    else if (option == 2)
+                    {
+
+                        new_account.Deposit();
+
+                        display_menu();
+                    }
+                    else if (option == 3)
+                    {
+                        new_account.withdraw();
+
+                        display_menu();
+                    }
+                    else if (option == 4)
+                    {
+
+                        new_account.get_details();
+
+                        display_menu();
+                    }
+                }
+
+                Console.WriteLine("You have exited");
+
+            }
+        }
+    }
---
+++ .NET/BANK_APP_USING_CLASS_FOR_SINGLE_ACCOUNT/BANK_APP_WITH_CLASS/account.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BANK_APP_USING_CLASS_FOR_SINGLE_ACCOUNT
+{
+    internal class account
+    {
+        int acc_no = 0;
+        string acc_name = string.Empty;
+        int balance = 0;
+        int deposit;
+        public account()
+        {
+        }
+
+        public account(int acc_no, string acc_name, int balance)
+        {
+            this.acc_no = acc_no;
+            this.acc_name = acc_name;
+            this.balance = balance;
+        }
+
+        public void Deposit()
+        {
+            Console.WriteLine("Enter deposit amount ");
+            deposit = Convert.ToInt32(Console.ReadLine());
+            if (deposit < 0)
+            {
+                Console.WriteLine("Invalid Amount");
+                while (deposit < 0)
+                {
+                    Console.WriteLine("please enter valid deposit amount ");
+                    deposit = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            balance = balance + deposit;
+            Console.WriteLine("Account Created Successfully \n");
+        }
+
+        public void withdraw()
+        {
+            Console.WriteLine("Enter amount to be withdrawed ");
+            int withdraw = Convert.ToInt32(Console.ReadLine());
+            if (withdraw < 0)
+            {
+                Console.WriteLine("Invalid Amount");
+                while (withdraw < 0)
+                {
+                    Console.WriteLine("please enter valid withdraw amount ");
+                    withdraw = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            if (withdraw <= balance)
+                balance = balance - withdraw;
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+                while (withdraw >= balance)
+                {
+                    Console.WriteLine("please enter valid withdraw amount ");
+                    withdraw = Convert.ToInt32(Console.ReadLine());
+                }
+                balance = balance - withdraw;
+            }
+            Console.WriteLine("Amount Withdrawed Successfully\n");
+        }
+
+        public void get_details()
+        {
+            Console.WriteLine("the account details are :");
+            Console.WriteLine("Account Number : " + acc_no);
+            Console.WriteLine("Account Holder Name : " + acc_name);
+            Console.WriteLine("Available Balance : " + balance);
+        }
+
+    }
+}
